d265726f32352bcb335640a4276e1891268278ae
Saisanjaychikkala <97425672+Saisanjaychikkala@users.noreply.github.com>
2022-07-01 21:01:44 +0530

    def count(num):     c=0     while num:         num=num//10         c+=1     return c n=int(input()) arr=list(map(int,input().split())) l=max(arr) c=count(l) co=0 for i in arr:     if count(i)==c:         co+=1 print(co)

---
+++ Max_digits_count.py
@@ -0,0 +1,15 @@
+def count(num):
+    c=0
+    while num:
+        num=num//10
+        c+=1
+    return c
+n=int(input())
+arr=list(map(int,input().split()))
+l=max(arr)
+c=count(l)
+co=0
+for i in arr:
+    if count(i)==c:
+        co+=1
+print(co)
