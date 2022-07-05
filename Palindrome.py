9472b54c782a4290f75901f68e53faf4054bfc68
Saisanjaychikkala <97425672+Saisanjaychikkala@users.noreply.github.com>
2022-07-05 17:54:57 +0530

    n=input() m=n[::-1] if m==n:     print(True) else:     print(False)

--- Palindrome.py
+++ Palindrome.py
@@ -1,12 +1,6 @@
-def reverse(num):
-    rev=0
-    while num:
-        rem=num%10
-        rev=rev*10+rem
-        num=num//10
-    return rev
-n=int(input())
-if reverse(n)==n:
+n=input()
+m=n[::-1]
+if m==n:
     print(True)
 else:
     print(False)
