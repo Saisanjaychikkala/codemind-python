1a36b9ba4cce428d29672777dc12d5891d6c4904
Saisanjaychikkala <97425672+Saisanjaychikkala@users.noreply.github.com>
2022-07-06 21:11:14 +0530

    def rev(a):     t=k=0     while a:         t=a%10         k=(k*10)+t         a=a//10     return k n=int(input()) k=rev(n) if n**2 == rev(k**2):     print("True") else:     print("False")

--- Adam_number.py
+++ Adam_number.py
@@ -1,15 +1,13 @@
-def reverse(num):
-    rev=0
-    while num:
-        rem=num%10
-        rev=rev*10+rem
-        num=num//10
-    return rev
-
-
-num=int(input())
-rev=reverse(num)
-if num**2==reverse(rev**2):
-    print(True)
+def rev(a):
+    t=k=0
+    while a:
+        t=a%10
+        k=(k*10)+t
+        a=a//10
+    return k
+n=int(input())
+k=rev(n)
+if n**2 == rev(k**2):
+    print("True")
 else:
-    print(False)
+    print("False")
