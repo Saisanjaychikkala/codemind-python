5b7a37f8b5220590757b2ed95cdaca4a98860051
Saisanjaychikkala <97425672+Saisanjaychikkala@users.noreply.github.com>
2022-07-06 19:23:37 +0530

    n=int(input()) for i in range(n-1,0,-1):     j=str(i)     if j==j[::-1]:         low=i         break for i in range(n+1,n**2):     j=str(i)     if j==j[::-1]:         high=i         break if((n-low)>(high-n)):     print(high) elif((n-low)<(high-n)):     print(low) else:     print(low,high)

--- Closest_Palindrome_.py
+++ Closest_Palindrome_.py
@@ -1,29 +1,17 @@
-def reverse(num):
-    rev=0
-    while num:
-        rem=num%10
-        rev=rev*10+rem
-        num=num//10
-    return rev
-    
-def lhs(num):
-    for i in range(num-1,0,-1):
-        if reverse(i)==i:
-            return i
- 
-def rhs(num):
-    div=num+1
-    while True:
-        if reverse(div)==div:
-            return div
-        div+=1
-
 n=int(input())
-small=lhs(n)
-big=rhs(n)
-if (n-small)<(big-n):
-    print(small)
-elif (n-small)>(big-n):
-    print(big)
+for i in range(n-1,0,-1):
+    j=str(i)
+    if j==j[::-1]:
+        low=i
+        break
+for i in range(n+1,n**2):
+    j=str(i)
+    if j==j[::-1]:
+        high=i
+        break
+if((n-low)>(high-n)):
+    print(high)
+elif((n-low)<(high-n)):
+    print(low)
 else:
-    print(small,big)
+    print(low,high)
