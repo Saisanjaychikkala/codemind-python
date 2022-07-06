a97537491583d59c94c2d6a63014bd63d90f13c4
Saisanjaychikkala <97425672+Saisanjaychikkala@users.noreply.github.com>
2022-07-06 20:16:40 +0530

    n=input() a=list(n) p,s=1,0 for i in n:     s+=int(i)**p     p+=1 if s==int(n):     print(True) else:     print(False)

--- Disarium_number.py
+++ Disarium_number.py
@@ -1,20 +1,10 @@
-def disarium(num):
-    le=len(str(num))
-    spd=0
-    temp=num
-    while num:
-        rem=num%10
-        spd+=rem**le
-        le-=1
-        num//=10
-    if temp==spd:
-        return True
-    else:
-        return False
-        
-
-num=int(input())
-if disarium(num):
+n=input()
+a=list(n)
+p,s=1,0
+for i in n:
+    s+=int(i)**p
+    p+=1
+if s==int(n):
     print(True)
 else:
     print(False)
