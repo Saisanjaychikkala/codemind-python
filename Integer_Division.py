34e3a10bb5b12683f82d04564a1c18ab336873f1
Saisanjaychikkala <97425672+Saisanjaychikkala@users.noreply.github.com>
2022-07-21 17:17:46 +0530

    class number:     def __init__(self,i):         self.x=i     def prime(self):         if self.x==1:             return 0         for i in range(2,int(self.x**0.5)+1):             if self.x%i==0:                 return 0         return 1     def __del__(self):         pass     def divide(self,divisor):         return self.x//divisor
    x=int(input())
    num=number(x)
    print(num.divide(10))

--- Integer_Division.py
+++ Integer_Division.py
@@ -1,2 +1,18 @@
-n=int(input())
-print(n//10)
+class number:
+    def __init__(self,i):
+        self.x=i
+    def prime(self):
+        if self.x==1:
+            return 0
+        for i in range(2,int(self.x**0.5)+1):
+            if self.x%i==0:
+                return 0
+        return 1
+    def __del__(self):
+        pass
+    def divide(self,divisor):
+        return self.x//divisor
+
+x=int(input())
+num=number(x)
+print(num.divide(10))
